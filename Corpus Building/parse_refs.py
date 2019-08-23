cd2815fd4a234269addfd66b3bf0e5e9357a21a0
Sarah SI MOUSSI <simoussi@SUPAGRO.INRA.LAN>
2019-08-23 13:49:45 +0200

    text mining launch

---
+++ Corpus Building/parse_refs.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 13:16:38 2019
+
+@author: simoussi
+"""
+
+import pandas as pd
+
+db_export=pd.read_csv("../known_interactions/globi.tsv",sep="\t")
